104bee5ef2d8d1c0fe5672503a105b88b3b2fd57
Per Hallström <per.hallstroem@thoughtworks.com>
2012-05-28 14:51:20 +0800

    Generating observations files

--- server/src/WorkGeneration/fits2workunits.py
+++ server/src/WorkGeneration/fits2workunits.py
@@ -1,28 +1,35 @@
 import math
 import pyfits
-import numpy as np
 
 MIN_LIVE_CHANNELS_PER_PIXEL = 3;
 INPUT_FILE = '/Users/perh/Dropbox/Documents/Work/ThoughtWorks/Projects/ICRAR/POGS_NGC628_v3.fits';
-GRID_SIZE = 5	
+GRID_SIZE = 5
 
 # The file is 3840x3840. Right now only looking at a small square where there is data
 START_X = 1800
 START_Y = 1800
-END_X = 1900
-END_Y = 1900
+END_X = 2000
+END_Y = 2000
 
 HDULIST = pyfits.open(INPUT_FILE);
 LAYER_COUNT = len(HDULIST)
 
 
-##
-##
+## ######################################################################## ##
+## 
 ## FUNCTIONS AND STUFF
 ## 
-##
+## ######################################################################## ##
+
+class Coordinate:
+	def __init__(self, x_coord, y_coord):
+		self.x = x_coord
+		self.y = y_coord
+	def __str__(obj):
+		return "<%(x)s, %(y)s>" % {'x':obj.x,'y':obj.y}
 
 def get_pixels(pix_x, pix_y):
+#	print "Getting pixels in <%(x)s, %(y)s>" % {'x':pix_x, 'y':pix_y} 
 	result = []
 	for x in pix_x:
 		if x >= END_X:
@@ -43,51 +50,60 @@ def get_pixels(pix_x, pix_y):
 			# data for only, say, 6 pixels but that has UV? Discussion with Kevin?
 			if live_pixels >= MIN_LIVE_CHANNELS_PER_PIXEL:
 #				print "%(galaxy)s__%(x)d_%(y)d " % { 'galaxy':object_name, 'x':x, 'y':y}
-
-				result.append(pixels)
+				coords = Coordinate(x, y) #"p%(x)s,%(y)s"%{'x':x,'y':y}
+				result.append({coords: pixels})
 	return result
-	
+		
 def squarify():
 	squares = []
 	for pix_y in range(START_Y, END_Y, GRID_SIZE):
+		print "Scanned %(pct_done)3d%% of image" % { 'pct_done':100*(pix_y-START_Y)/(END_Y-START_Y) }
 		pix_x = START_X
 		while pix_x < END_X:
 			pixels = get_pixels([pix_x], range(pix_y, pix_y+GRID_SIZE))
 			if len(pixels)>0:
-				squares.append([pix_x, pix_y]);
+				pixels.extend(get_pixels(range(pix_x+1, pix_x+GRID_SIZE), range(pix_y, pix_y+GRID_SIZE)))
+				coords = Coordinate(pix_x, pix_y) #"s%(x)s,%(y)s"%{'x':pix_x,'y':pix_y}
+				squares.append({coords:pixels});
 				pix_x+=GRID_SIZE;
 			else:
 				pix_x+=1
 	return squares
 
-def verify(squares):
-	result = []
-	distribution = [0 for i in range(GRID_SIZE * GRID_SIZE + 1)]
-	print "Verifying algorithm that returned %(x)d squares" % {'x':len(squares)}
-	for square in squares:
-		pixels = get_pixels(range(square[0], square[0]+GRID_SIZE), range(square[1], square[1]+GRID_SIZE))
-		pixel_count = len(pixels)
-		distribution[pixel_count] += 1
-		if pixel_count>0:
-			result.extend(pixels)
-
-	for i in range(GRID_SIZE * GRID_SIZE + 1):
-		print " -- %(pixels)2d pixels: %(count)7d squares" % { 'pixels':i, 'count':distribution[i] }
-	return "Found %(count)d pixels" % {'count':len(result) }
 	
-##
-##
+	
+## ######################################################################## ##
+## 
 ## Where it all starts
-##
-##
+## 
+## ######################################################################## ##
 
 #Here, it might be useful to assert that there are 12 input layers/channels/HDUs
 #print "List length: %(#)d" % {'#': len(HDULIST)} 
 
 object_name = HDULIST[0].header['OBJECT']
 
+squares = squarify()
+print "Workunits for: %(object)s" % { "object":object_name } 
 
-print verify(squarify())
+for square_list in squares:
+	for square in square_list:
+		print "  Square %(square)s has %(count)s pixels" % { 'square':square, 'count':len(square_list[square]) }
+		outfile = open("observations/obs%(object)s.%(sq_x)s.%(sq_y)s" % {'object':object_name, 'sq_x':square.x, 'sq_y':square.y}, 'w')
+		outfile.write("#  This workunit contains observations for object %(object)s\n" % { "object":object_name })
+		outfile.write("#  Square %(square)s contains %(count)s pixels with above-threshold observations\n" % {
+			'square':square, 'count':len(square_list[square]) })
+		outfile.write("#\n")
+
+		for pixel in square_list[square]:
+			for p in pixel.keys():
+#				print "    Pixel %(key)s => %(value)s" % { 'key':p, 'value':pixel[p]}
+				outfile.write("%(object)s~%(pix_x)s~%(pix_y)s" % {'object':object_name, 'pix_x':p.x, 'pix_y':p.y})
+				for one_value in pixel[p]:
+					outfile.write(" %(value)s" % { 'value':one_value })
+				outfile.write("\n");
+		outfile.close();
+
 print "================================ END OF OUTPUT ================================"
 
 # Uncomment to print general information about the file to stdout
