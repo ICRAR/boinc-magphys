5853cf1a459139edee5893a1c5026a172e5a6770
Per Hallström <per.hallstroem@thoughtworks.com>
2012-05-31 13:32:36 +0800

    pixels2wu: moved database support into separate file

---
+++ server/src/WorkGeneration/database_support.py
@@ -0,0 +1,71 @@
+import mysql.connector
+import mysql.connector.cursor
+import mysql.connector.errors
+
+class ConnectionHolder:
+	@classmethod
+	def getConnection(self):
+		if not hasattr(self, 'c'):
+			self.c = mysql.connector.connect(user='root', host='127.0.0.1', db='magphys_wu');
+		return self.c
+
+
+def doUpdate(conn, sql):
+	cursor = conn.cursor()
+	return cursor.execute(sql)
+	
+def fetchResultSet(conn, sql):
+	cursor = conn.cursor()
+	cursor.execute(sql)
+	rows = cursor.fetchall()
+	result = []
+	for row in rows:
+		if row is not None:
+			cols = [ d[0] for d in cursor.description ]
+			result.append(dict(zip(cols, row)))
+	return result
+	
+class ORMObject(object):
+	@classmethod
+	def _getById(self, id):
+		result_set = fetchResultSet(ConnectionHolder.getConnection(), "SELECT * FROM %(cls_name)s WHERE id = %(obj_id)d" % { 
+			'cls_name':self.__name__, 'obj_id':id })
+		return self(result_set[0])
+	
+	@classmethod
+	def _getByQuery(self, where):
+		result_set = fetchResultSet(ConnectionHolder.getConnection(), "SELECT * FROM %(cls_name)s WHERE %(where_clause)s" % {
+			'cls_name':self.__name__, 'where_clause':where })
+		
+		result = []
+		for row in result_set:
+			result.append(self(row))
+		return result;
+
+	def __init__(self, db_values):
+		for key in db_values.keys():
+			setattr(self, key, db_values[key])
+
+class Object(ORMObject):
+	def __str__(obj):
+		return "Object[%(id)d]<%(x)s x %(y)s x %(z)s>(%(name)s): %(desc)s" % {
+			'id':obj.id, 'x':obj.dimension_x,'y':obj.dimension_y,'z':obj.dimension_z,'name':obj.name,'desc':obj.description}
+
+class Square(ORMObject):
+	def getObject(self):
+		if not hasattr(self, 'object'):
+			self.object = Object._getById(self.object_id)
+		return self.object
+	def getPixels(self):
+		return Pixel._getByQuery("square_id=%(square_id)s" % {'square_id':self.id});
+	def __str__(obj):
+		return "Square[%(id)d]<%(x)s, %(y)s>(%(size)d x %(size)d)" % {'id':obj.id, 'x':obj.top_x,'y':obj.top_y,'size':obj.size}
+
+class Pixel(ORMObject):
+	def __str__(obj):
+		return "Pixel[%(id)d]<%(x)s, %(y)s>(square_id = %(sq_id)d, object_id = %(o_id)s" % {
+			'id':obj.id, 'x':obj.x,'y':obj.y,'sq_id':obj.square_id,'o_id':obj.object_id}
+	def getObject(self):
+		return Object._getById(self.object_id)
+	def getSquare(self):
+		return Square._getById(self.square_id)
--- server/src/WorkGeneration/pixels2wu.py
+++ server/src/WorkGeneration/pixels2wu.py
@@ -1,7 +1,5 @@
-import mysql.connector
-import mysql.connector.cursor
-import mysql.connector.errors
 import sys
+from database_support import *
 
 if(len(sys.argv) != 3):
 	print "usage:   %(me)s squares_to_process output_directory" % {'me':sys.argv[0]}
@@ -10,74 +8,6 @@ if(len(sys.argv) != 3):
 
 SQUARES_TO_PROCESS = sys.argv[1]
 OUTPUT_DIR = sys.argv[2]
-CONNECTION = mysql.connector.connect(user='root', host='127.0.0.1', db='magphys_wu');
-
-class ORMObject(object):
-	@classmethod
-	def _getById(self, id):
-		result_set = fetchResultSet(CONNECTION, "SELECT * FROM %(cls_name)s WHERE id = %(obj_id)d" % { 
-			'cls_name':self.__name__, 'obj_id':id })
-		return self(result_set[0])
-	
-	@classmethod
-	def _getByQuery(self, where):
-		result_set = fetchResultSet(CONNECTION, "SELECT * FROM %(cls_name)s WHERE %(where_clause)s" % {
-			'cls_name':self.__name__, 'where_clause':where })
-		
-		result = []
-		for row in result_set:
-			result.append(self(row))
-		return result;
-
-	def __init__(self, db_values):
-		for key in db_values.keys():
-			setattr(self, key, db_values[key])
-
-class Object(ORMObject):
-	def __str__(obj):
-		return "Object[%(id)d]<%(x)s x %(y)s x %(z)s>(%(name)s): %(desc)s" % {
-			'id':obj.id, 'x':obj.dimension_x,'y':obj.dimension_y,'z':obj.dimension_z,'name':obj.name,'desc':obj.description}
-
-class Square(ORMObject):
-	def getObject(self):
-		if not hasattr(self, 'object'):
-			self.object = Object._getById(self.object_id)
-		return self.object
-	def getPixels(self):
-		return Pixel._getByQuery("square_id=%(square_id)s" % {'square_id':self.id});
-	def __str__(obj):
-		return "Square[%(id)d]<%(x)s, %(y)s>(%(size)d x %(size)d)" % {'id':obj.id, 'x':obj.top_x,'y':obj.top_y,'size':obj.size}
-
-class Pixel(ORMObject):
-	def __str__(obj):
-		return "Pixel[%(id)d]<%(x)s, %(y)s>(square_id = %(sq_id)d, object_id = %(o_id)s" % {
-			'id':obj.id, 'x':obj.x,'y':obj.y,'sq_id':obj.square_id,'o_id':obj.object_id}
-	def getObject(self):
-		return Object._getById(self.object_id)
-	def getSquare(self):
-		return Square._getById(self.square_id)
-
-
-def fetchoneDict(cursor):
-	row = cursor.fetchone()
-	if row is None: return None
-	cols = [ d[0] for d in cursor.description ]
-	return dict(zip(cols, row))
-
-def doUpdate(conn, sql):
-	cursor = conn.cursor()
-	return cursor.execute(sql)
-	
-def fetchResultSet(conn, sql):
-	cursor = conn.cursor()
-	cursor.execute(sql)
-	rows = cursor.fetchall()
-	result = []
-	for row in rows:
-		if row is not None:
-			cols = [ d[0] for d in cursor.description ]
-			result.append(dict(zip(cols, row)))
-	return result
 
 def create_output_file(square):
 #	object = square.getObject().name
@@ -96,8 +26,8 @@ def create_output_file(square):
 			'object':square.getObject().name, 'pix_x':pixel.x, 'pix_y':pixel.y, 'pixel_values':pixel.pixel_values})
 	outfile.close();
 	update_query = "UPDATE square SET wu_generated=NOW() WHERE id=%(sq_id)s" % {'sq_id':square.id}
-	rows_updated = doUpdate(CONNECTION, update_query)
-	CONNECTION.commit()
+	rows_updated = doUpdate(ConnectionHolder.getConnection(), update_query)
+	ConnectionHolder.getConnection().commit()
 
 print "Fetching %(limit)s unprocessed squares..." % {'limit':SQUARES_TO_PROCESS}
 for square in Square._getByQuery("wu_generated IS NULL ORDER BY top_x+top_y ASC LIMIT %(limit)s" % {'limit':SQUARES_TO_PROCESS}):
