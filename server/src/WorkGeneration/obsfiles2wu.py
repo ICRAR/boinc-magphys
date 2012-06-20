517a18f51bed4f628a1de5b1a6ee5321954e1b50
Per Hallström <per.hallstroem@thoughtworks.com>
2012-06-20 15:41:24 +0800

    LOCALITY SCHEDULING: obsfiles2wu now gives the 100 first work units a higher priority

--- server/src/WorkGeneration/obsfiles2wu.py
+++ server/src/WorkGeneration/obsfiles2wu.py
@@ -14,29 +14,28 @@ BOINC_PROJECT_ROOT = sys.argv[2]
 BIN_PATH = BOINC_PROJECT_ROOT + "/bin"
 TEMPLATES_PATH = "templates" # In true BOINC style, this is magically relative to the project root
 
+NUMBER_OF_HIGH_PRIO_WORK_UNITS = 100			# Default number of work units to mark as high priority
+DEFAULT_HIGH_PRIORITY = "100"
 MIN_QUORUM = 4									# Validator run when there are at least this many results for a work unit
 TARGET_NRESULTS = MIN_QUORUM+2					# Initially create this many instances of a work unit
 DELAY_BOUND = 86400 * 30 						# Clients must report results within a month
-FPOPS_EST_PER_PIXEL = 1898						# Estimated number of gigaflops per pixel 
+FPOPS_EST_PER_PIXEL = 1.898						# Estimated number of gigaflops per pixel 
 FPOPS_BOUND_PER_PIXEL = FPOPS_EST_PER_PIXEL*15	# Maximum number of gigaflops per pixel client will allow before terminating job
-FPOPS_EXP = "e9"
+FPOPS_EXP = "e12"
 
 # The BOINC scripts/apps do not feel at home outside their directory
 os.chdir(BOINC_PROJECT_ROOT)
 
 file_list = os.listdir(FILE_DIR);
 
+files_processed = 0
 for file_name in file_list:
 	if file_name[0] == '.': continue	# Process everything but dot-files
 
 	pixels_in_file = sum(1 for line in open(FILE_DIR + "/" + file_name))-1
 	print("Creating work unit from observations file %(file)s: %(pixels)d pixels" % {'file':file_name, 'pixels':pixels_in_file})
 
-	new_full_path = check_output([BIN_PATH + "/dir_hier_path", file_name]).rstrip()	
-	os.rename(FILE_DIR + "/" + file_name, new_full_path)
-
-	cmd_create_work = [
-		BIN_PATH + "/create_work",
+	args_params = [
 		"--appname",         APP_NAME,
 		"--min_quorum",      "%(min_quorum)s" % {'min_quorum':MIN_QUORUM},
 		"--delay_bound",     "%(delay_bound)s" % {'delay_bound':DELAY_BOUND},
@@ -46,9 +45,23 @@ for file_name in file_list:
 		"--result_template", TEMPLATES_PATH + "/fitsed_result",
 		"--rsc_fpops_est",   "%(est)d%(exp)s" % {'est':FPOPS_EST_PER_PIXEL*pixels_in_file, 'exp':FPOPS_EXP},
 		"--rsc_fpops_bound", "%(bound)d%(exp)s"  % {'bound':FPOPS_BOUND_PER_PIXEL*pixels_in_file, 'exp':FPOPS_EXP},
-		file_name,
-		"filter_spec.dat", "zlibs.dat", "infrared_dce08_z0.0000.lbr", "starformhist_cb07_z0.0000.lbr"		
 	]
-	
+	args_files = [file_name, "filter_spec.dat", "zlibs.dat", "infrared_dce08_z0.0000.lbr", "starformhist_cb07_z0.0000.lbr"]
+
+	cmd_create_work = [
+		BIN_PATH + "/create_work"
+	]
+	cmd_create_work.extend(args_params)
+	if files_processed <= NUMBER_OF_HIGH_PRIO_WORK_UNITS:
+		cmd_create_work.extend(["--priority", DEFAULT_HIGH_PRIORITY])
+	cmd_create_work.extend(args_files)
+		
+	# Copy file into BOINC's download hierarchy	
+	new_full_path = check_output([BIN_PATH + "/dir_hier_path", file_name]).rstrip()	
+	os.rename(FILE_DIR + "/" + file_name, new_full_path)
+				
+	# And "create work" = create the work unit
 	if call(cmd_create_work):
 		print "Something went wrong; sorry"
+	
+	files_processed += 1
