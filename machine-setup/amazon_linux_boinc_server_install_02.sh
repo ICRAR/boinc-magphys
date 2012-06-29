3fd914046fbc57dca6edd77fed8e9afc577108ea
KevinVinsen <kvn1961@gmail.com>
2012-06-29 14:08:19 +0800

    It should be filters.dat not filter.dat

--- machine-setup/amazon_linux_boinc_server_install_02.sh
+++ machine-setup/amazon_linux_boinc_server_install_02.sh
@@ -8,6 +8,12 @@ cd /home/ec2-user/boinc-magphys/machine-setup
 # setup_website
 sudo rake setup_website
 
+# This is needed because the files that Apache serve are inside the user's home directory.
+chmod 711 /home/ec2-user
+chmod -R oug+r /home/ec2-user/projects/pogs
+chmod -R oug+x /home/ec2-user/projects/pogs/html
+chmod oug+w /home/ec2-user/projects/pogs/log_*
+
 # Copy files into place
 rake update_versions
 rake start_daemons
