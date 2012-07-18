829ec8341f5d20123014f657723075db34258fa7
KevinVinsen <kvn1961@gmail.com>
2012-07-18 16:45:50 +0800

    Removed all the BASH code and moved it to the Python

--- machine-setup/create_instance.py
+++ machine-setup/create_instance.py
@@ -8,12 +8,12 @@ import paramiko
 import glob
 import time
 
-IMAGE           = 'ami-aecd60c7' # Basic 64-bit Amazon Linux AMI - old
-KEY_NAME        = 'icrar-boinc'
-INSTANCE_TYPE   = 't1.micro'
+IMAGE = 'ami-aecd60c7' # Basic 64-bit Amazon Linux AMI
+KEY_NAME = 'icrar-boinc'
+INSTANCE_TYPE = 't1.micro'
 SECURITY_GROUPS = ['icrar-boinc-server'] # Security group allows SSH
-KEY_FILE        = os.path.expanduser('~/.ssh/icrar-boinc.pem')
-PUBLIC_KEYS     = os.path.expanduser('~/Documents/Keys')
+KEY_FILE = os.path.expanduser('~/.ssh/icrar-boinc.pem')
+PUBLIC_KEYS = os.path.expanduser('~/Documents/Keys')
 
 class CreateEc2Instance():
     def __init__(self):
@@ -21,10 +21,9 @@ class CreateEc2Instance():
         self._channel = None
         self._ssh = None
 
-    def setup(self, use_elastic_ip, public_ip, instance_name):
+    def setup(self, use_elastic_ip, public_ip):
         self._use_elastic_ip = use_elastic_ip
         self._public_ip = public_ip
-        self._instance_name = instance_name
 
     def close(self):
         self._channel.close()
@@ -58,6 +57,9 @@ class CreateEc2Instance():
         self._channel.send(command + '\n')
         self._wait_for_prompt()
 
+    def name_instance(self, instance_name):
+        self._conn.create_tags([self._instance.id], {"Name": instance_name})
+
     def stage01(self):
         # Create the EC2 instance
         print 'Starting an EC2 instance of type {0} with image {1}'.format(INSTANCE_TYPE, IMAGE)
@@ -79,7 +81,7 @@ class CreateEc2Instance():
             time.sleep(5)
         print '.'
 
-        if self._use_elastic_ip :
+        if self._use_elastic_ip:
             print 'Current DNS name is {0}. About to associate the Elastic IP'.format(self._instance.dns_name)
             if not self._conn.associate_address(instance_id=self._instance.id, public_ip=self._public_ip):
                 print 'Could not associate the IP {0} to the instance {1}'.format(self._public_ip, self._instance.id)
@@ -90,8 +92,6 @@ class CreateEc2Instance():
 
         # Load the new instance data as the dns_name may have changed
         self._instance.update(True)
-        self._conn.create_tags([self._instance.id], {"Name": self._instance_name})
-
         print 'Current DNS name is {0} after associating the Elastic IP'.format(self._instance.dns_name)
 
         # The instance is started, but not useable (yet)
@@ -115,7 +115,7 @@ class CreateEc2Instance():
         list_of_users = []
         for file in glob.glob(PUBLIC_KEYS + '/*.pub'):
             filename = os.path.basename(file)
-            user,ext = os.path.splitext(filename)
+            user, ext = os.path.splitext(filename)
             list_of_users.append(user)
             ftp.put(file, filename)
 
@@ -140,8 +140,36 @@ class CreateEc2Instance():
 
         # Now run the rest of the setup
         self._run_command('cd ~/boinc-magphys/machine-setup')
-        self._run_command('chmod +x *.sh')
-        self._run_command('./common_server_install.sh')
+
+        # Puppet and git should be installed by the python
+        self._run_command('sudo puppet boinc-magphys.pp')
+
+        # Recommended version per http://boinc.berkeley.edu/download_all.php on 2012-07-10
+        self._run_command('svn co http://boinc.berkeley.edu/svn/trunk/boinc /home/ec2-user/boinc')
+
+        self._run_command('cd /home/ec2-user/boinc')
+        self._run_command('./_autosetup')
+        self._run_command('./configure --disable-client --disable-manager')
+        self._run_command('make')
+
+        # Setup the pythonpath
+        self._run_command('''echo ' ' >> ~/.bash_profile''')
+        self._run_command('''echo 'PYTHONPATH=$PYTHONPATH:/home/ec2-user/boinc/py:/home/ec2-user/boinc-magphys/server/src' >> ~/.bash_profile''')
+        self._run_command('''echo 'export PYTHONPATH' >> ~/.bash_profile''')
+
+        # Setup the python
+        self._run_command('wget http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11-py2.7.egg')
+        self._run_command('sudo sh setuptools-0.6c11-py2.7.egg')
+        self._run_command('rm setuptools-0.6c11-py2.7.egg')
+        self._run_command('sudo rm -f /usr/bin/easy_install')
+        self._run_command('sudo easy_install-2.7 pip')
+        self._run_command('sudo rm -f /usr/bin/pip')
+        self._run_command('sudo pip-2.7 install sqlalchemy')
+        self._run_command('sudo pip-2.7 install Numpy')
+        self._run_command('sudo pip-2.7 install pyfits')
+
+        # Used by BOINC in the assimilator
+        self._run_command('sudo pip-2.7 install MySQL-python')
 
         for user in list_of_users:
             self._run_command('sudo useradd {0}'.format(user))
--- machine-setup/create_single_instance.py
+++ machine-setup/create_single_instance.py
@@ -4,6 +4,8 @@ Create a single instance with all the components running on the single instance
 
 # make sure we have the bits we need
 import sys
+import time
+
 try:
     import boto
     import paramiko
@@ -16,27 +18,80 @@ import getpass
 import create_instance
 import utils
 
+DB_USER = 'root'
+
 class CreateSingleEcsInstance(create_instance.CreateEc2Instance):
     def __init__(self):
         create_instance.CreateEc2Instance.__init__(self)
 
     def stage02(self):
-        # Finish the
-        self._run_command('./single_instance_install.sh')
+        # Finish the build
+        # Activate the DB
+        self._run_command('sudo mysql_install_db')
+        self._run_command('sudo chown -R mysql:mysql /var/lib/mysql/*')
+        self._run_command('''echo "service { 'mysqld': ensure => running, enable => true }" | sudo puppet apply''')
+
+        # Wait for it to start up
+        time.sleep(15)
+
+        # Make the POGS project
+        self._run_command('cd /home/ec2-user/boinc/tools')
+        self._run_command('yes | ./make_project -v --url_base http://{0} --db_user {1} pogs'.format(self._instance.dns_name, DB_USER))
+
+        # Setup the database for recording WU's
+        self._run_command('mysql --user={0} < /home/ec2-user/boinc-magphys/server/src/database/create_database.sql'.format(DB_USER))
+
+        # Build the validator
+        self._run_command('cd /home/ec2-user/boinc-magphys/server/src/Validator')
+        self._run_command('make')
+
+        # Move to the magphys area and start configuring
+        self._run_command('cd /home/ec2-user/boinc-magphys/machine-setup')
+
+        # setup_website
+        self._run_command('sudo rake setup_website')
+
+        # This is needed because the files that Apache serve are inside the user's home directory.
+        self._run_command('chmod 711 /home/ec2-user')
+        self._run_command('chmod -R oug+r /home/ec2-user/projects/pogs')
+        self._run_command('chmod -R oug+x /home/ec2-user/projects/pogs/html')
+        self._run_command('chmod ug+w /home/ec2-user/projects/pogs/log_*')
+        self._run_command('chmod ug+wx /home/ec2-user/projects/pogs/upload')
+
+        # Edit the files
+        self._run_command('cd /home/ec2-user/boinc-magphys/machine-setup')
+        self._run_command('python2.7 file_editor_single.py')
+
+        # Setup the forums
+        self._run_command('cd /home/ec2-user/projects/pogs/html/ops')
+        self._run_command('php create_forums.php')
+
+        # Copy files into place
+        self._run_command('cd /home/ec2-user/boinc-magphys/machine-setup')
+        self._run_command('rake update_versions')
+        self._run_command('rake start_daemons')
+
+        # Setup the crontab job to keep things ticking
+        self._run_command('crontab -l > /tmp/crontab.txt')
+        self._run_command('echo "0,5,10,15,20,25,30,35,40,45,50,55 * * * * cd /home/ec2-user/projects/pogs ; /home/ec2-user/projects/pogs/bin/start --cron" >> /tmp/crontab.txt')
+        self._run_command('crontab /tmp/crontab.txt')
 
         # Setup the ops area password
         self._run_command('cd /home/ec2-user/projects/pogs/html/ops')
         self._run_command('htpasswd -bc .htpasswd {0} {1}'.format(ops_username, ops_password))
 
-PUBLIC_IP       = '23.21.160.71'
-use_elastic_ip = utils.query_yes_no('Do you want to assign the Elastic IP [{0}] to this instance'.format(PUBLIC_IP), 'no')
+use_elastic_ip = utils.query_yes_no('Do you want to assign an Elastic IP to this instance', 'no')
+public_ip = None
+if use_elastic_ip:
+    public_ip = raw_input('What is the public IP address: ')
 ops_username = raw_input('Ops area username: ')
 ops_password = getpass.getpass('Password: ')
 instance_name = raw_input('Instance name: ')
 
 ec2_instance = CreateSingleEcsInstance()
-ec2_instance.setup(use_elastic_ip, PUBLIC_IP, instance_name)
+ec2_instance.setup(use_elastic_ip, public_ip)
 ec2_instance.stage01()
+ec2_instance.name_instance(instance_name)
 ec2_instance.stage02()
 ec2_instance.close()
 
