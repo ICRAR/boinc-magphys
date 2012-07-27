a12cc88b8bd2ef85babc9d75f634ba2c7eb01d5f
Rob Van der Sanden <robert.vandersanden@gmail.com>
2012-07-27 08:28:06 +0800

    Remove incorrectly added files

--- server/pogsproject/pogs/__init__.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database.database_support import login
-
-pogsEngine = create_engine(login)
-PogsSession = sessionmaker(bind=pogsEngine)
--- server/pogsproject/pogs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import DetailView, ListView
-from pogs.models import Galaxy
-
-urlpatterns = patterns('',
-    url(r'^$', 'pogs.views.galaxies'),
-    url(r'^UserGalaxy/(?P<userid>\d+)/(?P<galaxy_id>\d+)$', 'pogs.views.userGalaxy'),
-    url(r'^UserGalaxyImage/(?P<userid>\d+)/(?P<galaxy_id>\d+)/(?P<colour>\d+)$', 'pogs.views.userGalaxyImage'),
-)
