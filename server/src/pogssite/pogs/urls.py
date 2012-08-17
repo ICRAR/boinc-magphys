58f4a9c6ac108f610f85c9589d20294953b94f3a
Rob Van der Sanden <robert.vandersanden@gmail.com>
2012-08-17 08:36:53 +0800

    Redirect to the login page if no userid is passed

--- server/src/pogssite/pogs/urls.py
+++ server/src/pogssite/pogs/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
+from django.views.generic.simple import redirect_to
 from pogs.models import Galaxy
 
 urlpatterns = patterns('',
+    url(r'^$', redirect_to, {'url': '/pogs/login_form.php', 'permanent': False}),
     url(r'^(?P<userid>\d+)$', 'pogs.views.userGalaxies'),
     url(r'^UserGalaxy/(?P<userid>\d+)/(?P<galaxy_id>\d+)$', 'pogs.views.userGalaxy'),
     url(r'^UserGalaxyImage/(?P<userid>\d+)/(?P<galaxy_id>\d+)/(?P<colour>\d+)$', 'pogs.views.userGalaxyImage'),
