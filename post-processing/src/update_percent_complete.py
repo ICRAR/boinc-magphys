10d6f0efcbb3ab653ee648cb88d45b96f91559c4
KevinVinsen <kvn1961@gmail.com>
2012-09-24 14:48:29 +0800

    Now add the pixel code

--- post-processing/src/update_percent_complete.py
+++ post-processing/src/update_percent_complete.py
@@ -13,7 +13,7 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from config import db_login, boinc_db_login
 from database.boinc_database_support import Workunit
-from database.database_support import Galaxy, Area
+from database.database_support import Galaxy, Area, PixelResult
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC_FORMAT)
@@ -31,19 +31,33 @@ galaxies = session_magphys.query(Galaxy).all()
 for galaxy in galaxies:
     LOG.info('Working on galaxy %s (%d)', galaxy.name, galaxy.version_number)
 
+    # Look for whole areas first
     areas = session_magphys.query(Area).filter_by(galaxy_id=galaxy.galaxy_id).filter_by(workunit_id = None).all()
-
     for area in areas:
         wu_name = '{0}_area{1}'.format(galaxy.name, area.area_id)
         workunits = session_pogs.query(Workunit).filter_by(name=wu_name).all()
 
         for workunit in workunits:
             if workunit.assimilate_state == 2:
-                LOG.info('%s Found area %d - WU_id %d', galaxy.name, area.area_id, workunit.id)
+                LOG.info('%s Found area %d - WU_id %d - fixing', galaxy.name, area.area_id, workunit.id)
                 area.workunit_id = workunit.id
 
                 for pixel in area.pixelResults:
                     pixel.workunit_id = workunit.id
+
+    session_magphys.commit()
+
+    # Look for stray pixels
+    pixels = session_magphys.query(PixelResult).filter_by(galaxy_id=galaxy.galaxy_id).filter_by(workunit_id = None).all()
+    for pixel in pixels:
+        wu_name = '{0}_area{1}'.format(galaxy.name, pixel.area_id)
+        workunits = session_pogs.query(Workunit).filter_by(name=wu_name).all()
+
+        for workunit in workunits:
+            if workunit.assimilate_state == 2:
+                LOG.info('%s Found pixel % in area %d - WU_id %d - fixing', galaxy.name, pixel.pxresult_id, pixel.area_id, workunit.id)
+                pixel.workunit_id = workunit.id
+
 
     session_magphys.commit()
 
