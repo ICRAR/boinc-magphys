5adbb143915a7a6f70cb3c278ca15a8f5b2fcb8d
KevinVinsen <kvn1961@gmail.com>
2012-10-29 14:52:52 +0800

    Added a new tool

--- post-processing/src/tools/get_area_data.py
+++ post-processing/src/tools/get_area_data.py
@@ -28,7 +28,9 @@ Get the data about an area
 import argparse
 import logging
 from sqlalchemy.engine import create_engine
+from sqlalchemy.sql import select
 from config import DB_LOGIN
+from database.database_support_core import AREA, PIXEL_RESULT, PIXEL_PARAMETER, PARAMETER_NAME
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC_FORMAT)
@@ -40,3 +42,71 @@ args = vars(parser.parse_args())
 
 ENGINE = create_engine(DB_LOGIN)
 connection = ENGINE.connect()
+
+# Get the Parameter Map
+MAP_PARAMETER_NAMES = {}
+for parameter_name in connection.execute(select([PARAMETER_NAME])):
+    MAP_PARAMETER_NAMES[parameter_name[PARAMETER_NAME.c.parameter_name_id]] = parameter_name[PARAMETER_NAME.c.name]
+
+for area_id in args['area_id']:
+    area = connection.execute(select([AREA]).where(AREA.c.area_id == area_id)).first()
+
+    print '######################################'
+    print '######################################'
+    print '      Area  top_x  top_y  bottom_x  bottom_y  workunit_id'
+    print '{0:>10} {1:>6} {2:>6} {3:>9} {4:>9} {5:>12}'.format(area_id,
+        area[AREA.c.top_x],
+        area[AREA.c.top_y],
+        area[AREA.c.bottom_x],
+        area[AREA.c.bottom_y],
+        area[AREA.c.workunit_id])
+
+    for pixel_result in connection.execute(select([PIXEL_RESULT]).where(PIXEL_RESULT.c.area_id == area[AREA.c.area_id]).order_by(PIXEL_RESULT.c.pxresult_id)):
+        print '****'
+        #                    0     1     2            3         4        5       6         7         8         9      10      11         12          13           14      15       16        17          18          19       20       21       22         23         24
+        print '    pxresult_id     x     y  workunit_id     i_sfh     i_ir    chi2  redshift   fmu_sfh    fmu_ir      mu    tauv      s_sfr           m        ldust  t_w_bc  t_c_ism  xi_c_tot  xi_pah_tot  xi_mir_tot  x_w_tot    tvism    mdust        sfr      i_opt'
+        print '{0:>15} {1:>5} {2:>5} {3:>12} {4:>9} {5:>7} {6:>8} {7:>9} {8:>9} {9:>9} {10:>7} {11:>7} {12:>10} {13:>10} {14:>12} {15:>7} {16:>8} {17:>9} {18:>11} {19:>11} {20:>8} {21:>8} {22:>8} {23:>10} {24:>10}'.format(pixel_result[PIXEL_RESULT.c.pxresult_id],
+            pixel_result[PIXEL_RESULT.c.x],
+            pixel_result[PIXEL_RESULT.c.y],
+            pixel_result[PIXEL_RESULT.c.workunit_id],
+            pixel_result[PIXEL_RESULT.c.i_sfh],
+            pixel_result[PIXEL_RESULT.c.i_ir],
+            pixel_result[PIXEL_RESULT.c.chi2],
+            pixel_result[PIXEL_RESULT.c.redshift],
+            pixel_result[PIXEL_RESULT.c.fmu_sfh],
+            pixel_result[PIXEL_RESULT.c.fmu_ir],
+            pixel_result[PIXEL_RESULT.c.mu],
+            pixel_result[PIXEL_RESULT.c.tauv],
+            pixel_result[PIXEL_RESULT.c.s_sfr],
+            pixel_result[PIXEL_RESULT.c.m],
+            pixel_result[PIXEL_RESULT.c.ldust],
+            pixel_result[PIXEL_RESULT.c.t_w_bc],
+            pixel_result[PIXEL_RESULT.c.t_c_ism],
+            pixel_result[PIXEL_RESULT.c.xi_c_tot],
+            pixel_result[PIXEL_RESULT.c.xi_pah_tot],
+            pixel_result[PIXEL_RESULT.c.xi_mir_tot],
+            pixel_result[PIXEL_RESULT.c.x_w_tot],
+            pixel_result[PIXEL_RESULT.c.tvism],
+            pixel_result[PIXEL_RESULT.c.mdust],
+            pixel_result[PIXEL_RESULT.c.sfr],
+            pixel_result[PIXEL_RESULT.c.i_opt])
+
+        #                    00        01       02
+        print '          dmstar  dfmu_aux       dz'
+        print '{0:>15} {1:>9} {2:>8}'.format(pixel_result[PIXEL_RESULT.c.dmstar],
+            pixel_result[PIXEL_RESULT.c.dfmu_aux],
+            pixel_result[PIXEL_RESULT.c.dz])
+
+        print '******'
+        print ' parameter_name  percentile2_5  percentile16  percentile50  percentile84  percentile97_5  high_prob_bin  first_prob_bin  last_prob_bin  bin_step'
+        for pixel_parameter in connection.execute(select([PIXEL_PARAMETER]).where(PIXEL_PARAMETER.c.pxresult_id == pixel_result[PIXEL_RESULT.c.pxresult_id]).order_by(PIXEL_PARAMETER.c.parameter_name_id)):
+            print '{0:>15} {1:>14} {2:>13} {3:>13} {4:>13} {5:>15} {6:>14} {7:>15} {8:>14} {9:>9}'.format(MAP_PARAMETER_NAMES[pixel_parameter[PIXEL_PARAMETER.c.parameter_name_id]],
+                pixel_parameter[PIXEL_PARAMETER.c.percentile2_5],
+                pixel_parameter[PIXEL_PARAMETER.c.percentile16],
+                pixel_parameter[PIXEL_PARAMETER.c.percentile50],
+                pixel_parameter[PIXEL_PARAMETER.c.percentile84],
+                pixel_parameter[PIXEL_PARAMETER.c.percentile97_5],
+                pixel_parameter[PIXEL_PARAMETER.c.high_prob_bin],
+                pixel_parameter[PIXEL_PARAMETER.c.first_prob_bin],
+                pixel_parameter[PIXEL_PARAMETER.c.last_prob_bin],
+                pixel_parameter[PIXEL_PARAMETER.c.bin_step])
--- server/src/work_generation/delete_galaxy.py
+++ server/src/work_generation/delete_galaxy.py
@@ -30,12 +30,11 @@ from __future__ import print_function
 import argparse
 import logging
 import sys
-from sqlalchemy.sql.expression import func
 import time
 from config import DB_LOGIN
-from database.database_support import Galaxy, PixelFilter, PixelParameter, PixelHistogram, AreaUser, FitsHeader, Area, PixelResult
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
+from database.database_support_core import GALAXY, AREA, PIXEL_RESULT, PIXEL_FILTER, PIXEL_PARAMETER, PIXEL_HISTOGRAM, AREA_USER, FITS_HEADER
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC_FORMAT)
@@ -45,9 +44,8 @@ parser.add_argument('galaxy_id', nargs='+', help='the galaxy_id or 4-30 if you n
 args = vars(parser.parse_args())
 
 # First check the galaxy exists in the database
-engine = create_engine(DB_LOGIN)
-Session = sessionmaker(bind=engine)
-session = Session()
+ENGINE = create_engine(DB_LOGIN)
+connection = ENGINE.connect()
 
 galaxy_ids = None
 if len(args['galaxy_id']) == 1 and args['galaxy_id'][0].find('-') > 1:
@@ -58,38 +56,36 @@ else:
     galaxy_ids = args['galaxy_id']
 
 for galaxy_id_str in galaxy_ids:
+    transaction = connection.begin()
     galaxy_id1 = int(galaxy_id_str)
-    galaxy = session.query(Galaxy).filter_by(galaxy_id=galaxy_id1).first()
+    galaxy = connection.execute(select([GALAXY]).where(GALAXY.c.galaxy_id == galaxy_id1)).first()
     if galaxy is None:
         LOG.info('Error: Galaxy with galaxy_id of %d was not found', galaxy_id1)
     else:
-        LOG.info('Deleting Galaxy with galaxy_id of %d - %s', galaxy_id1, galaxy.name)
+        LOG.info('Deleting Galaxy with galaxy_id of %d - %s', galaxy_id1, galaxy[GALAXY.x.name])
 
-        values = session.query(func.min(Area.area_id),func.max(Area.area_id)).filter_by(galaxy_id=galaxy.galaxy_id).first()
-        LOG.info('Areas range {0}'.format(values))
-
-        for area_id1 in session.query(Area.area_id).filter_by(galaxy_id=galaxy.galaxy_id).order_by(Area.area_id).all():
-            for pxresult_id1 in session.query(PixelResult.pxresult_id).filter_by(area_id=area_id1[0]).order_by(PixelResult.pxresult_id).all():
+        for area_id1 in connection.execute(select([AREA.c.area_id]).where(AREA.c.galaxy_id == galaxy[GALAXY.c.galaxy_id]).order_by(AREA.c.area_id)):
+            for pxresult_id1 in connection.execute(select([PIXEL_RESULT.c.pxresult_id]).where(PIXEL_RESULT.c.area_id == area_id1[0]).order_by(PIXEL_RESULT.c.pxresult_id)):
                 print("Deleting galaxy {0} area {1} pixel {2}".format(galaxy_id_str, area_id1[0], pxresult_id1[0]), end="\r")
                 sys.stdout.flush()
 
-                session.query(PixelFilter).filter_by(pxresult_id=pxresult_id1[0]).delete()
-                session.query(PixelParameter).filter_by(pxresult_id=pxresult_id1[0]).delete()
-                session.query(PixelHistogram).filter_by(pxresult_id=pxresult_id1[0]).delete()
+                connection.execute(PIXEL_FILTER.delete().where(PIXEL_FILTER.cpxresult_id == pxresult_id1[0]))
+                connection.execute(PIXEL_PARAMETER.delete().where(PIXEL_PARAMETER.c.pxresult_id == pxresult_id1[0]))
+                connection.execute(PIXEL_HISTOGRAM.delete().where(PIXEL_HISTOGRAM.c.pxresult_id == pxresult_id1[0]))
 
-            session.query(PixelResult).filter_by(area_id=area_id1[0]).delete()
-            session.query(AreaUser).filter_by(area_id=area_id1[0]).delete()
+            connection.execute(PIXEL_RESULT.delete().where(PIXEL_RESULT.c.area_id == area_id1[0]))
+            connection.execute(AREA_USER.delete().where(AREA_USER.c.area_id == area_id1[0]))
 
-            session.commit()
+            transaction.commit()
+            transaction = connection.begin()
 
-            # Give the rest of the world a chance to access the database
+    # Give the rest of the world a chance to access the database
             time.sleep(10)
 
-        session.query(Area).filter_by(galaxy_id=galaxy.galaxy_id).delete()
-        session.query(FitsHeader).filter_by(galaxy_id=galaxy.galaxy_id).delete()
-        session.delete(galaxy)
+        connection.execute(AREA.delete().where(AREA.c.galaxy_id == galaxy[GALAXY.c.galaxy_id]))
+        connection.execute(FITS_HEADER.delete().where(FITS_HEADER.c.galaxy_id == galaxy[GALAXY.c.galaxy_id]))
+        connection.execute(GALAXY.delete().where(GALAXY.c.galaxy_id == galaxy[GALAXY.c.galaxy_id]))
         LOG.info('Galaxy with galaxy_id of %d was deleted', galaxy_id1)
-    session.commit()
+    transaction.commit()
 
-session.commit()
-session.close()
+connection.close()
