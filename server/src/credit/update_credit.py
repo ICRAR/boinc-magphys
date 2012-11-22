1ee60003e670e8c6ad58eaed188fd3ba0727c2f7
KevinVinsen <kvn1961@gmail.com>
2012-11-22 09:36:15 +0800

    Getting the PYTHONPATH right

--- server/src/credit/update_credit.py
+++ server/src/credit/update_credit.py
@@ -37,7 +37,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC
 # Setup the Python Path as we may be running this via ssh
 base_path = os.path.dirname(__file__)
 sys.path.append(os.path.abspath(os.path.join(base_path, '..')))
-sys.path.append(os.path.abspath(os.path.join(base_path, '../../../boinc/py')))
+sys.path.append(os.path.abspath(os.path.join(base_path, '../../../../boinc/py')))
 LOG.info('PYTHONPATH = {0}'.format(sys.path))
 
 from config import DB_LOGIN
