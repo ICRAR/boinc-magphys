3378374695e832f661606711abc8fa2ac0733f1b
KevinVinsen <kvn1961@gmail.com>
2013-08-01 15:04:11 +0800

    Reformat the filename to have 0's in it

--- server/src/archive/archive_boinc_stats_mod.py
+++ server/src/archive/archive_boinc_stats_mod.py
@@ -72,11 +72,22 @@ def process_boinc():
         ec2_helper.run_instance(USER_DATA, BOINC_VALUE)
 
 
+def correct(directory_name):
+    """
+    Correct the directory name so it has 0's in front of single digit numbers
+    :param directory_name:
+    :return:
+    """
+    add_zeros = lambda string: '{0:02d}'.format(int(string))
+    elements = directory_name.split('_')
+    return '{0}_{1}_{2}_{3}_{4}_{5}_{6}'.format(elements[0], elements[1], add_zeros(elements[2]), add_zeros(elements[3]), add_zeros(elements[4]), add_zeros(elements[5]), add_zeros(elements[6]))
+
+
 def move_files_to_s3(s3helper, directory_name):
     for file_name in glob.glob(os.path.join(directory_name, '*')):
         (root_directory_name, tail_directory_name) = os.path.split(directory_name)
         (root_file_name, tail_file_name) = os.path.split(file_name)
-        key = get_stats_archive_key(tail_directory_name, tail_file_name)
+        key = get_stats_archive_key(correct(tail_directory_name), tail_file_name)
         LOG.info('Adding {0} to {1}'.format(file_name, key))
         s3helper.add_file_to_bucket(get_archive_bucket(), key, file_name)
 
