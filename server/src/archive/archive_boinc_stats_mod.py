254556f81a27615f8f886a9b4668f6995d2aa413
KevinVinsen <kvn1961@gmail.com>
2013-08-01 13:42:39 +0800

    Check the log files

--- server/src/archive/archive_boinc_stats_mod.py
+++ server/src/archive/archive_boinc_stats_mod.py
@@ -78,7 +78,7 @@ def move_files_to_s3(s3helper, directory_name):
         key = get_stats_archive_key(tail_directory_name, tail_file_name)
         s3helper.add_file_to_bucket(get_archive_bucket(), key, file_name)
 
-    os.removedirs(directory_name)
+    #os.removedirs(directory_name)
 
 
 def process_ami():
