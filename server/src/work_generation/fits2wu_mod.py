53a4b81a0f1612b655585769dabe99b2de59a8e3
KevinVinsen <kvn1961@gmail.com>
2013-11-04 06:49:49 +0800

    Debugging tag code

--- server/src/work_generation/fits2wu_mod.py
+++ server/src/work_generation/fits2wu_mod.py
@@ -712,4 +712,5 @@ class Fit2Wu:
         :return:
         """
         for tag_register in self._connection.execute(select([TAG_REGISTER]).where(TAG_REGISTER.c.register_id == register_id)):
+            LOG.info('tag_id: {0}, galaxy_id: {1}, register_id: {2}'.format(tag_register[TAG_REGISTER.c.tag_id], self._galaxy_id, register_id))
             self._connection.execute(TAG_GALAXY.insert().values(galaxy_id=self._galaxy_id, tag_id=tag_register[TAG_REGISTER.c.tag_id]))
