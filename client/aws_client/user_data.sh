cd00c47e1e8dc75de0e467fd8f2f5cfffb7596f5
KevinVinsen <kvn1961@gmail.com>
2013-11-22 12:18:27 +0800

    User data for the aws client

--- client/aws_client/user_data.sh
+++ client/aws_client/user_data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/boinc/client/
-sudo -u ec2-user -c "./boinc --daemon"
+su --session-command "./boinc --daemon" ec2-user
 
 # Pause to let things get running
 sleep 120s
 
 # Attach
-sudo -u ec2-user -c "./boinccmd --project_attach http://pogs.theskynet.org/pogs/ <authenticator>"
-
+su --session-command "./boinccmd --project_attach http://pogs.theskynet.org/pogs/ <authenticator>" ec2-user
