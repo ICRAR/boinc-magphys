42460fe3f63160e2db041ac78983fa0646878f33
sam6321 <sam6321@live.com.au>
2015-01-29 13:37:00 +0800

    Moved over changes from develop

--- server/src/work_generation/register_fits_file.py
+++ server/src/work_generation/register_fits_file.py
@@ -27,16 +27,17 @@
 Register a FITS file ready to be converted into Work Units
 """
 
-import os, sys
+import os, sys, shutil
 base_path = os.path.dirname(__file__)
 sys.path.append(os.path.abspath(os.path.join(base_path, '..')))
 
-import argparse
+import argparse, time
 
 from utils.logging_helper import config_logger
 
 from sqlalchemy.engine import create_engine
 from config import DB_LOGIN
+#DB_LOGIN = 'sqlite:////home/ict310/Desktop/register_fits_file.db'
 
 from work_generation.register_fits_file_mod import fix_redshift, get_data_from_galaxy_txt, \
     decompress_gz_files, extract_tar_file, find_input_filename, find_sigma_filename, add_to_database, \
@@ -46,6 +47,7 @@ LOG = config_logger(__name__)
 LOG.info('PYTHONPATH = {0}'.format(sys.path))
 
 parser = argparse.ArgumentParser()
+parser.add_argument('working_directory', nargs=1, help='galaxies directory')
 parser.add_argument('TAR_file', nargs=1, help='the input tar containing the galaxies')
 parser.add_argument('TXT_file', nargs=1, help='the input text file containing galaxy summaries')
 parser.add_argument('priority', type=int, nargs=1, help='the higher the number the higher the priority')
@@ -53,7 +55,7 @@ parser.add_argument('run_id', type=int, nargs=1, help='the run id to be used')
 parser.add_argument('tags', nargs='*', help='any tags to be associated with the galaxy')
 
 args = vars(parser.parse_args())
-
+WORKING_DIRECTORY = args['working_directory'][0]
 INPUT_FILE = args['TAR_file'][0]
 PRIORITY = args['priority'][0]
 RUN_ID = args['run_id'][0]
@@ -65,39 +67,56 @@ if not os.path.isfile(INPUT_FILE):
     LOG.error('The file %s does not exist', INPUT_FILE)
     exit(1)
 
+if not WORKING_DIRECTORY.endswith('/'):
+    WORKING_DIRECTORY += '/'
+
 # Extract all files from the tar file if they are not already extracted
-TAR_EXTRACT_LOCATION = INPUT_FILE[:-4]
+(head, tail) = os.path.split(INPUT_FILE)
 
-extract_tar_file(INPUT_FILE, TAR_EXTRACT_LOCATION)
+TAR_EXTRACT_LOCATION = WORKING_DIRECTORY + os.path.splitext(tail)[0]
+LOG.info('Working Directory: {0}'.format(WORKING_DIRECTORY))
+LOG.info('TAR Extract Location: {0}'.format(TAR_EXTRACT_LOCATION))
+time.sleep(5)
 
-decompress_gz_files(TAR_EXTRACT_LOCATION)
+num_files_extracted = extract_tar_file(INPUT_FILE, TAR_EXTRACT_LOCATION)
 
-move_fits_files(TAR_EXTRACT_LOCATION, '..')
-TAR_EXTRACT_LOCATION = '.'
+num_files_decompressed = decompress_gz_files(TAR_EXTRACT_LOCATION)
 
 all_txt_file_data = get_data_from_galaxy_txt(GALAXY_TEXT_FILE)
+num_galaxies_in_txt = len(all_txt_file_data)
+
+num_unused_fits = clean_unused_fits(TAR_EXTRACT_LOCATION, all_txt_file_data)
+
+move_fits_files(TAR_EXTRACT_LOCATION, WORKING_DIRECTORY)
+
+shutil.rmtree(TAR_EXTRACT_LOCATION)
 
 all_galaxy_data = []
+
+num_galaxies_without_file = 0
+num_galaxies_without_sigma = 0
 
 for txt_line_info in all_txt_file_data:
     single_galaxy_data = dict()
     single_galaxy_data['name'] = txt_line_info[0]
 
-    input_file = find_input_filename(txt_line_info[0], TAR_EXTRACT_LOCATION)
+    input_file = find_input_filename(txt_line_info[0], WORKING_DIRECTORY)
     if input_file is None:
         LOG.error('Galaxy {0} has an input file of None!'.format(single_galaxy_data['name']))
+        num_galaxies_without_file += 1
         continue
 
-    sigma = find_sigma_filename(txt_line_info[0], TAR_EXTRACT_LOCATION)
+    sigma = find_sigma_filename(txt_line_info[0], WORKING_DIRECTORY)
     if sigma is None:
         LOG.error('Galaxy {0} has a sigma file of None!'.format(single_galaxy_data['name']))
-        continue
+        num_galaxies_without_sigma += 1
+
 
     gal_type = txt_line_info[4]
     if gal_type is '':
         gal_type = 'Unk'
 
-    single_galaxy_data['sigma'] = find_sigma_filename(txt_line_info[0], TAR_EXTRACT_LOCATION)
+    single_galaxy_data['sigma'] = sigma
     single_galaxy_data['redshift'] = float(fix_redshift(txt_line_info[3]))
     single_galaxy_data['input_file'] = input_file
     single_galaxy_data['type'] = gal_type
@@ -107,18 +126,26 @@ for txt_line_info in all_txt_file_data:
 
     all_galaxy_data.append(single_galaxy_data)
 
-save_data_to_file(all_galaxy_data, 'GalaxyRun1.txt')
-
-clean_unused_fits(TAR_EXTRACT_LOCATION, all_galaxy_data)
-
 # Connect to the database - the login string is set in the database package
 ENGINE = create_engine(DB_LOGIN)
 connection = ENGINE.connect()
 
+num_galaxies_inserted = 0
+
 for galaxy in all_galaxy_data:
     try:
         add_to_database(connection, galaxy)
+        num_galaxies_inserted += 1
     except Exception:
         LOG.exception('An error occurred adding {0} to the database'.format(galaxy['name']))
 
 connection.close()
+
+LOG.info('Summary information: ')
+LOG.info('Total files extracted from tar: {0}'.format(num_files_extracted))
+LOG.info('Total gz files decompressed: {0}'.format(num_files_decompressed))
+LOG.info('Total galaxies defined in text file: {0}'.format(num_galaxies_in_txt))
+LOG.info('Total fits files without an entry in text file: {0}'.format(num_unused_fits))
+LOG.info('Total galaxies in text document with no fits file: {0}'.format(num_galaxies_without_file))
+LOG.info('Total galaxies without a sigma file: {0}'.format(num_galaxies_without_sigma))
+LOG.info('Total galaxies inserted into database: {0}'.format(num_galaxies_inserted))
