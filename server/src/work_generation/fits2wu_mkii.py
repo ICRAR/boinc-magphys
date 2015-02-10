7356a14fbae5ed1d1e90e2b643f1bc776af751ec
sam6321 <sam6321@live.com.au>
2015-02-10 10:02:41 +0800

    Added div by 0 checking

--- server/src/work_generation/fits2wu_mkii.py
+++ server/src/work_generation/fits2wu_mkii.py
@@ -142,7 +142,7 @@ else:
                     connection.execute(REGISTER.update().where(REGISTER.c.register_id == registration[REGISTER.c.register_id]).values(create_time=datetime.now()))
                 connection.execute(TAG_REGISTER.delete().where(TAG_REGISTER.c.register_id == registration[REGISTER.c.register_id]))
 
-        if len(areaave) > 0
+        if len(areaave) > 0:
             LOG.info('Total db time: {0}'.format(total_db_time))
             asum = 0
             for rtime in areaave:
