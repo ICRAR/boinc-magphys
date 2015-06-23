ab08213b4b76a9cd2a8a770f52025976c7f83eaa
sam6321 <sam6321@live.com.au>
2015-06-23 12:38:02 +0800

    Bug fixing

--- server/src/hdf5_to_fits/hdf5_to_fits_mod.py
+++ server/src/hdf5_to_fits/hdf5_to_fits_mod.py
@@ -164,8 +164,11 @@ def get_features_layers_galaxies(connection, request_id):
             layers.append('sfr_0_1gyr')
 
     hdf5_request_galaxy_ids = []
-    for galaxy_request in connection.execute(select([HDF5_REQUEST_GALAXY], distinct=True).where(HDF5_REQUEST_GALAXY.c.hdf5_request_id == request_id and HDF5_REQUEST_GALAXY.c.state == 0)):
-        hdf5_request_galaxy_ids.append(HDF5RequestDetails(galaxy_request[HDF5_REQUEST_GALAXY.c.hdf5_request_galaxy_id], galaxy_request[HDF5_REQUEST_GALAXY.c.galaxy_id]))
+    for galaxy_request in connection.execute(select([HDF5_REQUEST_GALAXY], distinct=True).where(HDF5_REQUEST_GALAXY.c.hdf5_request_id == request_id)):
+        state = galaxy_request[HDF5_REQUEST_GALAXY.c.state]
+        LOG.info('State {0}'.format(state))
+        if state is not 0:
+            hdf5_request_galaxy_ids.append(HDF5RequestDetails(galaxy_request[HDF5_REQUEST_GALAXY.c.hdf5_request_galaxy_id], galaxy_request[HDF5_REQUEST_GALAXY.c.galaxy_id]))
 
     return features, layers, hdf5_request_galaxy_ids
 
