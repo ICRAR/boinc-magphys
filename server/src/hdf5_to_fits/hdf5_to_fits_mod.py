f35ea350cf157566806af473d69502a64b4f4113
sam6321 <sam6321@live.com.au>
2015-06-23 11:47:20 +0800

    Bug fixing

--- server/src/hdf5_to_fits/hdf5_to_fits_mod.py
+++ server/src/hdf5_to_fits/hdf5_to_fits_mod.py
@@ -275,7 +275,7 @@ def generate_files(connection, hdf5_request_galaxy_ids, email, features, layers)
     # Check whether all the requested galaxies are available or not.
     for hdf5_request_galaxy in hdf5_request_galaxy_ids:
         galaxy = connection.execute(select([GALAXY]).where(GALAXY.c.galaxy_id == hdf5_request_galaxy.galaxy_id)).first()
-        LOG.info('State = {0}'.format(hdf5_request_galaxy.state))
+        LOG.info('State = {0}'.format(hdf5_request_galaxy.c.state))
 
         key = get_key_hdf5(galaxy[GALAXY.c.name], galaxy[GALAXY.c.run_id], galaxy[GALAXY.c.galaxy_id])
 
